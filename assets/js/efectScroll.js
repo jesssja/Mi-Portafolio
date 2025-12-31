2be06b7b5b43b7007f1dbd4d32975da8edf94546
jesssja <jessjabby1011@gmail.com>
2025-12-31 17:37:33 -0600

    cambiar propiedad reset,en scrollReveal para form de contacto

--- assets/js/efectScroll.js
+++ assets/js/efectScroll.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal('.contacto-container', {
     distance: '500px',
     duration: 2000,
     easing: 'ease-in-out',
-    reset: true,
+    reset: false,
 });
 
 ScrollReveal().reveal('.iconos-contacto', {
